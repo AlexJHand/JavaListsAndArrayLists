b3799ab80b3caf5164cbd7aa0aef771a9a2296a1
Alex Hand <alex.j.hand@gmail.com>
2018-03-05 11:00:18 -0600

    Added printInstructions

--- src/com/company/GroceryList.java
+++ src/com/company/GroceryList.java
@@ -29,5 +29,10 @@ public class GroceryList {
     public String findItem(String searchItem) {
 //        boolean exists groceryList.contains(searchItem);
         int position = groceryList.indexOf(searchItem);
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+
+        return null;
     }
 }
